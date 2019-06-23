a4bb880c9e12e78bd9010819b24e36a88984c484
Akihiro TAKASE <42milez@gmail.com>
2019-06-23 23:15:37 +0900

    Clean up

--- modules/enet/networked_multiplayer_enet.h
+++ modules/enet/networked_multiplayer_enet.h
@@ -1,33 +1,3 @@
-/*************************************************************************/
-/*  networked_multiplayer_enet.h                                         */
-/*************************************************************************/
-/*                       This file is part of:                           */
-/*                           GODOT ENGINE                                */
-/*                      https://godotengine.org                          */
-/*************************************************************************/
-/* Copyright (c) 2007-2019 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2019 Godot Engine contributors (cf. AUTHORS.md)    */
-/*                                                                       */
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the       */
-/* "Software"), to deal in the Software without restriction, including   */
-/* without limitation the rights to use, copy, modify, merge, publish,   */
-/* distribute, sublicense, and/or sell copies of the Software, and to    */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions:                                             */
-/*                                                                       */
-/* The above copyright notice and this permission notice shall be        */
-/* included in all copies or substantial portions of the Software.       */
-/*                                                                       */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
-/*************************************************************************/
-
 #ifndef NETWORKED_MULTIPLAYER_ENET_H
 #define NETWORKED_MULTIPLAYER_ENET_H
 
@@ -36,10 +6,7 @@
 
 #include <enet/enet.h>
 
-class NetworkedMultiplayerENet : public NetworkedMultiplayerPeer {
-
-	GDCLASS(NetworkedMultiplayerENet, NetworkedMultiplayerPeer);
-
+class NetworkedMultiplayerENet : public NetworkedMultiplayerPeer { GDCLASS(NetworkedMultiplayerENet, NetworkedMultiplayerPeer);
 public:
 	enum CompressionMode {
 		COMPRESS_NONE,
@@ -48,6 +15,61 @@ public:
 		COMPRESS_ZLIB,
 		COMPRESS_ZSTD
 	};
+
+protected:
+	static void _bind_methods();
+
+public:
+	NetworkedMultiplayerENet();
+	~NetworkedMultiplayerENet();
+
+    Error create_client(const String &p_address, int p_port, int p_in_bandwidth = 0, int p_out_bandwidth = 0, int p_client_port = 0);
+	Error create_server(int p_port, int p_max_clients = 32, int p_in_bandwidth = 0, int p_out_bandwidth = 0);
+
+	void close_connection(uint32_t wait_usec = 100);
+	void disconnect_peer(int p_peer, bool now = false);
+	
+	virtual void poll();
+
+	virtual Error get_packet(const uint8_t **r_buffer, int &r_buffer_size); ///< buffer is GONE after next get_packet
+	virtual Error put_packet(const uint8_t *p_buffer, int p_buffer_size);
+
+	bool is_always_ordered() const;
+	virtual bool is_refusing_new_connections() const;
+	virtual bool is_server() const;
+
+public:
+	virtual int get_available_packet_count() const;
+	int get_channel_count() const;
+	CompressionMode get_compression_mode() const;
+	virtual ConnectionStatus get_connection_status() const;
+	int get_last_packet_channel() const;
+	virtual int get_max_packet_size() const;
+	int get_packet_channel() const;
+	virtual int get_packet_peer() const;
+	virtual IP_Address get_peer_address(int p_peer_id) const;
+	virtual int get_peer_port(int p_peer_id) const;
+	int get_transfer_channel() const;
+	virtual TransferMode get_transfer_mode() const;
+	virtual int get_unique_id() const;
+
+	void set_always_ordered(bool p_ordered);
+	void set_bind_ip(const IP_Address &p_ip);
+	void set_channel_count(int p_channel);
+	void set_compression_mode(CompressionMode p_mode);
+	virtual void set_refuse_new_connections(bool p_enable);
+	virtual void set_target_peer(int p_peer);
+	void set_transfer_channel(int p_channel);
+	virtual void set_transfer_mode(TransferMode p_mode);
+
+private:
+	static size_t enet_compress(void *context, const ENetBuffer *inBuffers, size_t inBufferCount, size_t inLimit, enet_uint8 *outData, size_t outLimit);
+	static void enet_compressor_destroy(void *context);
+	static size_t enet_decompress(void *context, const enet_uint8 *inData, size_t inLimit, enet_uint8 *outData, size_t outLimit);
+
+	uint32_t _gen_unique_id() const;
+	void _pop_current_packet();
+	void _setup_compressor();
 
 private:
 	enum {
@@ -62,107 +84,39 @@ private:
 		SYSCH_MAX
 	};
 
-	bool active;
-	bool server;
-
-	uint32_t unique_id;
-
-	int target_peer;
-	TransferMode transfer_mode;
-	int transfer_channel;
-	int channel_count;
-	bool always_ordered;
-
-	ENetEvent event;
-	ENetPeer *peer;
-	ENetHost *host;
-
-	bool refuse_connections;
-
-	ConnectionStatus connection_status;
-
-	Map<int, ENetPeer *> peer_map;
-
 	struct Packet {
-
 		ENetPacket *packet;
 		int from;
 		int channel;
 	};
 
-	CompressionMode compression_mode;
+	Map<int, ENetPeer *> peer_map;
 
 	List<Packet> incoming_packets;
-
-	Packet current_packet;
-
-	uint32_t _gen_unique_id() const;
-	void _pop_current_packet();
 
 	Vector<uint8_t> src_compressor_mem;
 	Vector<uint8_t> dst_compressor_mem;
 
-	ENetCompressor enet_compressor;
-	static size_t enet_compress(void *context, const ENetBuffer *inBuffers, size_t inBufferCount, size_t inLimit, enet_uint8 *outData, size_t outLimit);
-	static size_t enet_decompress(void *context, const enet_uint8 *inData, size_t inLimit, enet_uint8 *outData, size_t outLimit);
-	static void enet_compressor_destroy(void *context);
-	void _setup_compressor();
-
 	IP_Address bind_ip;
+	CompressionMode compression_mode;
+	ConnectionStatus connection_status;
+	Packet current_packet;
+	ENetCompressor enet_compressor;
+	ENetEvent event;
+	ENetHost *host;
+	ENetPeer *peer;
+	TransferMode transfer_mode;
 
-protected:
-	static void _bind_methods();
+	uint32_t unique_id;
 
-public:
-	virtual void set_transfer_mode(TransferMode p_mode);
-	virtual TransferMode get_transfer_mode() const;
-	virtual void set_target_peer(int p_peer);
+	int channel_count;
+	int target_peer;
+	int transfer_channel;
 
-	virtual int get_packet_peer() const;
-
-	virtual IP_Address get_peer_address(int p_peer_id) const;
-	virtual int get_peer_port(int p_peer_id) const;
-
-	Error create_server(int p_port, int p_max_clients = 32, int p_in_bandwidth = 0, int p_out_bandwidth = 0);
-	Error create_client(const String &p_address, int p_port, int p_in_bandwidth = 0, int p_out_bandwidth = 0, int p_client_port = 0);
-
-	void close_connection(uint32_t wait_usec = 100);
-
-	void disconnect_peer(int p_peer, bool now = false);
-
-	virtual void poll();
-
-	virtual bool is_server() const;
-
-	virtual int get_available_packet_count() const;
-	virtual Error get_packet(const uint8_t **r_buffer, int &r_buffer_size); ///< buffer is GONE after next get_packet
-	virtual Error put_packet(const uint8_t *p_buffer, int p_buffer_size);
-
-	virtual int get_max_packet_size() const;
-
-	virtual ConnectionStatus get_connection_status() const;
-
-	virtual void set_refuse_new_connections(bool p_enable);
-	virtual bool is_refusing_new_connections() const;
-
-	virtual int get_unique_id() const;
-
-	void set_compression_mode(CompressionMode p_mode);
-	CompressionMode get_compression_mode() const;
-
-	int get_packet_channel() const;
-	int get_last_packet_channel() const;
-	void set_transfer_channel(int p_channel);
-	int get_transfer_channel() const;
-	void set_channel_count(int p_channel);
-	int get_channel_count() const;
-	void set_always_ordered(bool p_ordered);
-	bool is_always_ordered() const;
-
-	NetworkedMultiplayerENet();
-	~NetworkedMultiplayerENet();
-
-	void set_bind_ip(const IP_Address &p_ip);
+	bool active;
+	bool always_ordered;
+	bool refuse_connections;
+	bool server;
 };
 
 VARIANT_ENUM_CAST(NetworkedMultiplayerENet::CompressionMode);
