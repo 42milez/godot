713741c76a800e59e4628e0cba2ee12e0fd40bbc
Akihiro TAKASE <42milez@gmail.com>
2020-01-23 15:55:30 +0900

    clean up

--- thirdparty/enet/enet/godot.h
+++ thirdparty/enet/enet/godot.h
@@ -51,16 +51,23 @@ typedef void *ENetSocket;
 
 #define ENET_SOCKET_NULL NULL
 
-#define ENET_HOST_TO_NET_16(value) (htons(value)) /**< macro that converts host to net byte-order of a 16-bit value */
-#define ENET_HOST_TO_NET_32(value) (htonl(value)) /**< macro that converts host to net byte-order of a 32-bit value */
+#define ENET_HOST_TO_NET_16(value)                                           \
+  (htons(value)) /**< macro that converts host to net byte-order of a 16-bit \
+                    value */
+#define ENET_HOST_TO_NET_32(value)                                           \
+  (htonl(value)) /**< macro that converts host to net byte-order of a 32-bit \
+                    value */
 
-#define ENET_NET_TO_HOST_16(value) (ntohs(value)) /**< macro that converts net to host byte-order of a 16-bit value */
-#define ENET_NET_TO_HOST_32(value) (ntohl(value)) /**< macro that converts net to host byte-order of a 32-bit value */
+#define ENET_NET_TO_HOST_16(value)                                           \
+  (ntohs(value)) /**< macro that converts net to host byte-order of a 16-bit \
+                    value */
+#define ENET_NET_TO_HOST_32(value)                                           \
+  (ntohl(value)) /**< macro that converts net to host byte-order of a 32-bit \
+                    value */
 
-typedef struct
-{
-	void *data;
-	size_t dataLength;
+typedef struct {
+  void *data;
+  size_t dataLength;
 } ENetBuffer;
 
 #define ENET_CALLBACK
