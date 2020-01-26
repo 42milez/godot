57e6a2f13197a79457691d10b3443f952ddf8b7a
Akihiro TAKASE <42milez@gmail.com>
2020-01-27 00:03:31 +0900

    clean up

--- thirdparty/enet/protocol.c
+++ thirdparty/enet/protocol.c
@@ -1886,8 +1886,7 @@ int enet_host_service(ENetHost *host, ENetEvent *event, enet_uint32 timeout) {
   timeout += host->serviceTime;
 
   do {
-    if (ENET_TIME_DIFFERENCE(host->serviceTime, host->bandwidthThrottleEpoch) >=
-        ENET_HOST_BANDWIDTH_THROTTLE_INTERVAL)
+    if (ENET_TIME_DIFFERENCE(host->serviceTime, host->bandwidthThrottleEpoch) >= ENET_HOST_BANDWIDTH_THROTTLE_INTERVAL)
       enet_host_bandwidth_throttle(host);
 
     switch (enet_protocol_send_outgoing_commands(host, event, 1)) {
